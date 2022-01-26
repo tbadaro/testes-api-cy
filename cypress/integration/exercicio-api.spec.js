c2cb39645750bc7696c2999450fe876d8b729330
Tadeu Badaró <t.badaro@gmail.com>
2022-01-26 02:03:43 -0300

    testes de usuário criados em  exercicio-api.spec.js; faker carregado.

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -1,30 +1,105 @@
 /// <reference types="cypress" />
+import { fake } from 'faker';
+import contrato from '../contracts/usuarios.contract'
+var faker = require('faker');
+faker.locale = 'pt_BR';
 
 describe('Testes da Funcionalidade Usuários', () => {
-
+     let token
+     before(() => {
+         cy.token('fulano@qa.com', 'teste').then(tkn => { token = tkn })
+     });
+ 
     it('Deve validar contrato de usuários', () => {
-         //TODO: 
+          cy.request('usuarios').then(response => {
+               return contrato.validateAsync(response.body)
+          })
     });
 
     it('Deve listar usuários cadastrados', () => {
-         //TODO: 
+          cy.request({
+               method: 'GET',
+               url: 'usuarios'
+          }).then((response) => {          
+               expect(response.status).to.equal(200)
+               expect(response.body).to.have.property('usuarios')
+               expect(response.duration).to.be.lessThan(20)
+          })
     });
 
     it('Deve cadastrar um usuário com sucesso', () => {
-         //TODO: 
+          let nomeFaker = faker.name.firstName()+" "+faker.name.lastName()
+          let emailFaker = faker.internet.email()
+          let senhaFaker = faker.internet.password()
+          let adminFaker = faker.datatype.boolean()
+          
+          cy.cadastrarUsuario(nomeFaker, emailFaker, senhaFaker, `${adminFaker}`)
+               .then((response) => {
+                    expect(response.status).to.equal(201)
+                    expect(response.body.message).to.equal('Cadastro realizado com sucesso')
+               })   
+          
     });
 
     it('Deve validar um usuário com email inválido', () => {
-         //TODO: 
+          let nomeFaker = faker.name.firstName()+" "+faker.name.lastName()
+          let senhaFaker = faker.internet.password()
+          let adminFaker = faker.datatype.boolean()
+
+          cy.cadastrarUsuario(nomeFaker, "fulano@qa.com", senhaFaker, `${adminFaker}`)
+          .then((response) => {
+               expect(response.status).to.equal(400)
+               expect(response.body.message).to.equal('Este email já está sendo usado')
+          })   
     });
 
     it('Deve editar um usuário previamente cadastrado', () => {
-         //TODO: 
+          let nomeFaker = faker.name.firstName()+" "+faker.name.lastName()
+          let emailFaker = faker.internet.email()     
+          let senhaFaker = faker.internet.password()
+          let adminFaker = faker.datatype.boolean()
+          
+          cy.cadastrarUsuario(nomeFaker, emailFaker, senhaFaker, `${adminFaker}`)
+               .then(response => {
+                    let id = response.body._id
+               
+                    cy.request({
+                         method: "PUT",
+                         url: `usuarios/${id}`,
+                         body:
+                         {
+                              "nome": nomeFaker,
+                              "email": emailFaker,
+                              "password": senhaFaker,
+                              "administrador": `${adminFaker}`
+                         }
+                    }).then((response) => {
+                         expect(response.status).to.equal(200)
+                         expect(response.body.message).to.equal('Registro alterado com sucesso')
+                    })
+               })
+          
     });
 
     it('Deve deletar um usuário previamente cadastrado', () => {
-        //TODO: 
-    });
+          let nomeFaker = faker.name.firstName()+" "+faker.name.lastName()
+          let emailFaker = faker.internet.email()     
+          let senhaFaker = faker.internet.password()
+          let adminFaker = faker.datatype.boolean()
+          
+          cy.cadastrarUsuario(nomeFaker, emailFaker, senhaFaker, `${adminFaker}`)
+               .then(response => {
+                    let id = response.body._id                    
+                    cy.request({
+                         method: "DELETE",
+                         url: `usuarios/${id}`
+                    }).then(response =>{
+                         expect(response.body.message).to.equal('Registro excluído com sucesso')
+                         expect(response.status).to.equal(200)
+                     })
+
+               });
+     });
 
 
-});
+})
